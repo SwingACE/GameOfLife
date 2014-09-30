d8ef04ba030fd3feb1e71abbe9c7a4dddb2381f1
Smoudy <bogeliu@126.com>
2014-09-30 21:26:06 +0800

    Create interface.js

---
+++ interface.js
@@ -0,0 +1,31 @@
+//绘制网格中的线条
+function drawGrid (){
+	var edge = sizeOfCell*sizeOfGrid;
+	var sh = edge.toString();
+	map.lineWidth = 2;
+    map.beginPath();
+	for (var i = 0; i < sizeOfGrid; i++){
+		map.moveTo(0,i*sizeOfCell);
+		map.lineTo(edge,i*sizeOfCell);
+	}
+	map.moveTo(0,edge);
+	map.lineTo(edge,edge);
+	for (var i = 0; i < sizeOfGrid; i++){
+		map.moveTo(i*sizeOfCell,0);
+		map.lineTo(i*sizeOfCell,edge);
+	}
+	map.moveTo(edge,0);
+	map.lineTo(edge,edge);
+	map.closePath();
+	map.stroke();
+}
+
+//根据细胞状态给网格上色
+function paintGrid (){
+	for (var i = 0; i < sizeOfGrid; i++){
+		for (var j = 0; j < sizeOfGrid; j++){
+			map.fillStyle = (Grid.grid[i][j] == 1) ? "#000000" : "#C0C0C0";
+			map.fillRect(j*sizeOfCell+2,i*sizeOfCell+2,sizeOfCell-4,sizeOfCell-4);
+		}
+	}
+}
